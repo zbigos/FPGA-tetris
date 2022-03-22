ab45ea0ddf59d64db559b1290047683f347b3b47
zerotoasic <zerotoasic@zerotoasic.vm.com>
2022-03-22 05:46:56 +0100

    fix

--- src/scoredisplay.v
+++ src/scoredisplay.v
@@ -94,7 +94,7 @@ module scoredisplay(
     assign inside_cell1 = (vreadwire >= 375-3 & vreadwire < 417-3) & (hreadwire >= 567 & hreadwire < 623); 
     wire [11:0] pixstream1;
     assign pixstream1 = inside_cell1 ? __pixstream1 : 12'b000000000000;
-    score_digit s1(.clk_25_175(clk_25_175), .reset(reset), .mut_hreadwire(hreadwire - 567), .mut_vreadwire(vreadwire - 372), .pixstream(__pixstream1), .value(valused[0] ? values[0] : 4'b1111));
+    score_digit s1(.clk_25_175(clk_25_175), .reset(reset), .mut_hreadwire(hreadwire - 7'd567), .mut_vreadwire(vreadwire - 7'd372), .pixstream(__pixstream1), .value(valused[0] ? values[0] : 4'b1111));
 
 
     wire [11:0] __pixstream2;
@@ -102,48 +102,48 @@ module scoredisplay(
     assign inside_cell2 = (vreadwire >= 329-3 & vreadwire < 370-3) & (hreadwire >= 567 & hreadwire < 623); 
     wire [11:0] pixstream2;
     assign pixstream2 = inside_cell2 ? __pixstream2 : pixstream1;
-    score_digit s2(.clk_25_175(clk_25_175), .reset(reset), .mut_hreadwire(hreadwire - 567), .mut_vreadwire(vreadwire - 326), .pixstream(__pixstream2), .value(valused[1] ? values[1] : 4'b1111));
+    score_digit s2(.clk_25_175(clk_25_175), .reset(reset), .mut_hreadwire(hreadwire - 7'd567), .mut_vreadwire(vreadwire - 7'd326), .pixstream(__pixstream2), .value(valused[1] ? values[1] : 4'b1111));
 
     wire [11:0] __pixstream3;
     wire inside_cell3;
     assign inside_cell3 = (vreadwire >= 283-3 & vreadwire < 324-3) & (hreadwire >= 567 & hreadwire < 623); 
     wire [11:0] pixstream3;
-    score_digit s3(.clk_25_175(clk_25_175), .reset(reset), .mut_hreadwire(hreadwire - 567), .mut_vreadwire(vreadwire - 280), .pixstream(__pixstream3), .value(valused[2] ? values[2] : 4'b1111));
+    score_digit s3(.clk_25_175(clk_25_175), .reset(reset), .mut_hreadwire(hreadwire - 7'd567), .mut_vreadwire(vreadwire - 7'd280), .pixstream(__pixstream3), .value(valused[2] ? values[2] : 4'b1111));
     assign pixstream3 = inside_cell3 ? __pixstream3 : pixstream2;
 
     wire [11:0] __pixstream4;
     wire inside_cell4;
     assign inside_cell4 = (vreadwire >= 237-3 & vreadwire < 278-3) & (hreadwire >= 567 & hreadwire < 623); 
     wire [11:0] pixstream4;
-    score_digit s4(.clk_25_175(clk_25_175), .reset(reset), .mut_hreadwire(hreadwire - 567), .mut_vreadwire(vreadwire - 234), .pixstream(__pixstream4), .value(valused[3] ? values[3] : 4'b1111));
+    score_digit s4(.clk_25_175(clk_25_175), .reset(reset), .mut_hreadwire(hreadwire - 7'd567), .mut_vreadwire(vreadwire - 7'd234), .pixstream(__pixstream4), .value(valused[3] ? values[3] : 4'b1111));
     assign pixstream4 = inside_cell4 ? __pixstream4 : pixstream3;
 
     wire [11:0] __pixstream5;
     wire inside_cell5;
     assign inside_cell5 = (vreadwire >= 191-3 & vreadwire < 232-3) & (hreadwire >= 567 & hreadwire < 623); 
     wire [11:0] pixstream5;
-    score_digit s5(.clk_25_175(clk_25_175), .reset(reset), .mut_hreadwire(hreadwire - 567), .mut_vreadwire(vreadwire - 188), .pixstream(__pixstream5), .value(valused[4] ? values[4] : 4'b1111));
+    score_digit s5(.clk_25_175(clk_25_175), .reset(reset), .mut_hreadwire(hreadwire - 7'd567), .mut_vreadwire(vreadwire - 7'd188), .pixstream(__pixstream5), .value(valused[4] ? values[4] : 4'b1111));
     assign pixstream5 = inside_cell5 ? __pixstream5 : pixstream4;
 
     wire [11:0] __pixstream6;
     wire inside_cell6;
     assign inside_cell6 = (vreadwire >= 145-3 & vreadwire < 186-3) & (hreadwire >= 567 & hreadwire < 623); 
     wire [11:0] pixstream6;
-    score_digit s6(.clk_25_175(clk_25_175), .reset(reset), .mut_hreadwire(hreadwire - 567), .mut_vreadwire(vreadwire - 142), .pixstream(__pixstream6), .value(valused[5] ? values[5] : 4'b1111));
+    score_digit s6(.clk_25_175(clk_25_175), .reset(reset), .mut_hreadwire(hreadwire - 7'd567), .mut_vreadwire(vreadwire - 7'd142), .pixstream(__pixstream6), .value(valused[5] ? values[5] : 4'b1111));
     assign pixstream6 = inside_cell6 ? __pixstream6 : pixstream5;
 
     wire [11:0] __pixstream7;
     wire inside_cell7;
     assign inside_cell7 = (vreadwire >= 99-3 & vreadwire < 140-3) & (hreadwire >= 567 & hreadwire < 623); 
     wire [11:0] pixstream7;
-    score_digit s7(.clk_25_175(clk_25_175), .reset(reset), .mut_hreadwire(hreadwire - 567), .mut_vreadwire(vreadwire - 96), .pixstream(__pixstream7), .value(valused[6] ? values[6] : 4'b1111));
+    score_digit s7(.clk_25_175(clk_25_175), .reset(reset), .mut_hreadwire(hreadwire - 7'd567), .mut_vreadwire(vreadwire - 7'd96), .pixstream(__pixstream7), .value(valused[6] ? values[6] : 4'b1111));
     assign pixstream7 = inside_cell7 ? __pixstream7 : pixstream6;
 
     wire ech_inside;
     assign ech_inside = (vreadwire >= 42 & vreadwire < 422) & (hreadwire >= 562 & hreadwire < 628); 
     wire [11:0] __pixstream8;
     wire inside_cell8;
-    score_digit s8(.clk_25_175(clk_25_175), .reset(reset), .mut_hreadwire(hreadwire - 567), .mut_vreadwire(vreadwire - 50), .pixstream(__pixstream8), .value(valused[7] ? values[7] : 4'b1111));
+    score_digit s8(.clk_25_175(clk_25_175), .reset(reset), .mut_hreadwire(hreadwire - 7'd567), .mut_vreadwire(vreadwire - 7'd50), .pixstream(__pixstream8), .value(valused[7] ? values[7] : 4'b1111));
     assign inside_cell8 = (vreadwire >= 53-3 & vreadwire < 94-3) & (hreadwire >= 567 & hreadwire < 623); 
     assign pixstream = inside_cell8 ? __pixstream8 : (ech_inside ? pixstream7 : 12'b111111111111);
 
